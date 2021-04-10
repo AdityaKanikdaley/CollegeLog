527eb44f262e1ec774a0423226e29f6da1071e1e
chinmay189jain <chinmayjain.189@gmail.com>
2021-04-11 00:04:38 +0530

    Commit from Chinmay

---
+++ app/src/main/java/com/example/collegelog/SavedButtonDB.java
@@ -0,0 +1,8 @@
+package com.example.collegelog;
+
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteDatabase;
+
+public class SavedButtonDB {
+    //Hello Aditya from Petlu
+}
